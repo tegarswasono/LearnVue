b21ef4fa62c8465744f4428011426686bc011fbf
tegarswasono <tegar.s@weefer.co.id>
2023-12-12 08:43:11 +0700

    add Bootstrap

---
+++ LearnVueRouter/src/components/Bootstrap/Bootstrap.vue
@@ -0,0 +1,8 @@
+<script setup>
+    import "bootstrap/dist/css/bootstrap.min.css"
+    import "bootstrap"
+</script>
+<template>
+    <button class="btn btn-danger">Button</button> <br/>
+    <button class="btn btn-sm btn-info">Button</button>
+</template>
