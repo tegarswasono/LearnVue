e215bf817f9e3e9a0d8527356d892c735e50b247
tegarswasono <tegar.s@weefer.co.id>
2023-12-14 17:09:56 +0700

    add learntypescript

---
+++ LearnVueTypeScript/src/views/LearnTypescript/PrimitiveDataType.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    let firstName2 = "Dylan"
+    let firstName: string = "Dylan"
+
+    let BoolType: boolean = true
+    let numberType: number = 123456789012345678901234567890
+    let stringType: string = 'John Doe'
+
+    let bigintType: bigint = BigInt(12345)
+    let symbolType: symbol = Symbol.for('Csv column title');
+</script>
+<template>
+    {{ firstName }}
+    {{ firstName2 }}
+    <hr/>
+
+    {{ BoolType }} || 
+    {{ numberType }} || 
+    {{ stringType }} || 
+    {{ bigintType }} || 
+    {{ symbolType }}
+</template>
