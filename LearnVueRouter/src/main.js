2314f3a2d628c737246bf816570341623a8bc140
tegarswasono <tegar.s@weefer.co.id>
2023-12-14 16:48:58 +0700

    fix learnvuerouter

--- LearnVueRouter/src/main.js
+++ LearnVueRouter/src/main.js
@@ -1,7 +1,8 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './components/Bootstrap/Bootstrap.vue'
+//import App from './components/Bootstrap/Bootstrap.vue'
+import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
